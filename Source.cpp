3d9debe92a1c46961603168bf1690de05a96cae5
Fedorov <chugun777akk@mail.ru>
2023-03-24 09:42:29 +0700

    think over the conditions

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    static int searchInsert(std::vector<int>& nums, int target) {
+        int positionCounter = 0;
+        if (nums.empty()|| target==0)
+            return 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            positionCounter++;
+            if (nums[i] == target)             // продумать условия 
+            {
+                return i;
+            }
+            else if(target< nums[i] && target>nums[i-1])
+            {
+                return i;
+            }
+            
+        }
+        return positionCounter;
+    }
+    
+};
+
+
+
+
+
+int main()
+{
+    std::vector<int> vec1 = { 1,3,5,6 }; // 2
+    int aim1 = 5;
+    std::vector<int> vec2 = { 1,3,5,6 }; //1
+    int aim2 = 0;
+    std::vector<int> vec3 = { 1,3,5,6 }; //4
+    int aim3 = 7;
+    std::vector<int> vec4 = { 2,5 };
+    int aim4 = 1;
+    
+    std::cout << Solution::searchInsert(vec4, aim4);
+    return 0;
+}
